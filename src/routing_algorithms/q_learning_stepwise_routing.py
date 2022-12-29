84b7e7b2291a37e581e0789bfce8abbdb4fe459f
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-12-29 18:45:49 +0100

    Link quality function optimized

--- src/routing_algorithms/q_learning_stepwise_routing.py
+++ src/routing_algorithms/q_learning_stepwise_routing.py
@@ -18,7 +18,7 @@ class QlearningStepwiseRouting(BASE_routing):
         self.RMIN = 0
 
         self.random = np.random.RandomState(self.simulator.seed) #it generates a random value to be used in epsilon greedy
-        self.link_qualities = {} #In order to calculate it, we considered the packet transmission time and packet delivery ratio. To calculate this, the Window Mean with Exponentially Weighted Moving Average (WMEWMA) method[9] was used. Note: for the moment I consider the distance between node.
+        self.link_qualities = {} #In order to calculate it, we should considered the packet transmission time and packet delivery ratio but we made a simplification considering only the distance between two drones.
         self.q_table = {} #{drone_1: [drone_1, drone_2, ..., drone_n], .., drone_n: [drone_1, drone_2, ..., drone_n]}
         self.link_stability = {}
         self.old_state = None
@@ -36,7 +36,7 @@ class QlearningStepwiseRouting(BASE_routing):
         @return:
         """
 
-    #TO CHANGE!
+    #Function which is called at each step and it is used to compute the reward for each drone. After a reward has been chosen, another function is called to update the qtable.
     def compute_reward(self, drone):
         reward = None
         distance_cur_drone_depot = util.euclidean_distance(drone.coords, self.simulator.depot.coords)
@@ -70,13 +70,12 @@ class QlearningStepwiseRouting(BASE_routing):
         """
         
         self.check_state(self.drone.identifier)
-        #self.update_link_quality(self.drone) #WORKS GOOD BUT NEED OPTIMIZATION!
+        self.update_link_quality() #WORKS GOOD BUT NEED OPTIMIZATION!
         #print(self.link_qualities)
         drones_speed = self.compute_nodes_speed(self.drone, self.simulator.drones)
         link_quality_sum = {}
         for drone in self.simulator.drones:
-            #link_quality_sum[drone.identifier] = self.compute_past_link_quality(drone)
-            link_quality_sum[drone.identifier] = 1
+            link_quality_sum[drone.identifier] = self.sum_n_last_link_qualities(drone)
             self.link_stability[drone.identifier] = (1-self.BETA)*np.exp(1/drones_speed[drone.identifier])+self.BETA*(link_quality_sum[drone.identifier]/len(self.simulator.drones))
         
         self.old_state = self.drone
@@ -102,24 +101,31 @@ class QlearningStepwiseRouting(BASE_routing):
         self.q_table[state][action] = (1-self.LEARNING_RATE)*self.q_table[state][action] + self.LEARNING_RATE*(reward + (self.DISCOUNT_FACTOR * max(self.q_table[next_state])))
 
 
-    #Save the link quality of the drone at current step
-    def update_link_quality(self, cur_drone):
-        starting_point = 0 if (self.simulator.cur_step < config.RETRANSMISSION_DELAY) or (len(self.link_qualities.keys()) == 0) else max(self.link_qualities.keys()) #self.simulator.cur_step-config.RETRANSMISSION_DELAY
-        for step in range(starting_point, self.simulator.cur_step):
-            i = cur_drone
-            link_qualities = []
-            for j in self.simulator.drones:
-                link_quality_ij = np.exp(-7*(util.euclidean_distance(i.coords, j.coords)/config.COMMUNICATION_RANGE_DRONE)) if i != j else 0
-                link_qualities.append(link_quality_ij)
+    #Save the link quality of the drone at current step. Only the last n link qualities are saved (where n is the number of drones in the simulator)
+    def update_link_quality(self):
+        #Why do we need a starting point?
+        #Due to the retransmission delay, it seems that the relay selection function is only called every "RETRANSMISSION_DELAY" times. So, for example, it may happen that if "RETRANSMISSION_DELAY"=10
+        #we have that link quality is only computed every 10 steps (e.g. 10, 20, 30, etc..) but since we need it in all steps we necessarily have to make an approcimation and assign the same link quality
+        #at every steps in each retransmission interval (e.g. 0/9, 10/19, etc..). Starting point is the point from which we have to assign the last computed link quality.
+        #So, at the first steps (self.simulator.cur_step < config.RETRANSMISSION_DELAY or the link quality is still empty) we assign 0 as the starting point, otherwise we start from the last step
+        #for which we have computed the link quality.
+        starting_point = 0 if (self.simulator.cur_step < config.RETRANSMISSION_DELAY) or (len(self.link_qualities.keys()) == 0) else max(self.link_qualities.keys())
+        i = self.drone
+        link_qualities = []
+        for j in self.simulator.drones: #For each drones
+            #We compute the link quality between i (self.drone) and the other j drones that depends on the distance. We use an exponential decay function so the closer they are, the higher the quality.
+            link_quality_ij = np.exp(-7*(util.euclidean_distance(i.coords, j.coords)/config.COMMUNICATION_RANGE_DRONE)) if i != j else 0
+            link_qualities.append(link_quality_ij) #We store the qualities between i and j
+        for step in range(starting_point, self.simulator.cur_step): #Finally we assign the computed link qualities to each step from starting point to the current step.
             self.link_qualities[step] = link_qualities
+        while (len(self.link_qualities.keys()) > self.simulator.n_drones): #Since we need only the last n link qualities, we delete the others (saves a lot of memory!)
+            min_step = np.min(list(self.link_qualities.keys()))
+            del self.link_qualities[min_step]
 
-
-    def compute_past_link_quality(self, neighbor):
-        link_quality = 0
-        sum_lower_bound = self.simulator.cur_step-self.simulator.n_drones if self.simulator.cur_step >= self.simulator.n_drones else 0
-        for k in range(sum_lower_bound, self.simulator.cur_step-1):
-            link_quality += self.link_qualities[k][neighbor.identifier]
-        return link_quality
+    #Compute the sum of the n last link qualities between self and and another drone (usually a neighbor) (n is the number of drones in the simulator). The output is used to compute the link stability.
+    def sum_n_last_link_qualities(self, drone):
+        #Since we keep only the last n link qualities computed in the link quality dictionary, we simply sum the all link quality values for the given drone
+        return sum(link_qualities[drone.identifier] for link_qualities in self.link_qualities.values())
 
     #TO CHANGE TOO! (how to compute? - 𝑣_i,j represents the speed at which nodes 𝑖 and 𝑗 are moving away)
     def compute_nodes_speed(self, cur_drone, neighbors):
